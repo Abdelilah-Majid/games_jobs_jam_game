df5248fd6e4b098fc6ed4258188c975e768c12e0
abdelilah majid <ain@pop-os.localdomain>
2021-02-23 13:04:58 +0100

    adding the players movment and some tile examples

---
+++ src/code/players_actor.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL:=Vector2.UP
+export var speed=Vector2(300.0,1000.0)
+export var gravity:=4000.0
+var _velocity:=Vector2.ZERO
+
+
+func _physics_process(delta: float) -> void:
+	var is_jump_interrupted:=Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var direction:=get_direction()
+	_velocity=calculate_move_velocity(direction,speed,_velocity,is_jump_interrupted)
+	_velocity=move_and_slide(_velocity, FLOOR_NORMAL)
+	return
+
+
+
+func get_direction()->Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+	)
+
+
+func calculate_move_velocity(direction:Vector2,speed:Vector2,linear_velocity:Vector2,is_jump_interrupted:bool)->Vector2:
+	var new_velocity:=linear_velocity
+	
+	new_velocity.x=speed.x*direction.x
+	new_velocity.y+=gravity*get_physics_process_delta_time()
+	
+	if direction.y ==-1.0:
+		new_velocity.y=direction.y*speed.y
+	
+	if is_jump_interrupted:
+		new_velocity.y=0.0
+	
+	return new_velocity
+
+
