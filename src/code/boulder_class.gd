3caf968ed376485a7011d24b277cddb1c3c02503
abdelilah majid <ain@pop-os.localdomain>
2021-02-24 20:50:14 +0100

    fixing a bug that is related to the robot for the moving rocks function

---
+++ src/code/boulder_class.gd
@@ -0,0 +1,5 @@
+extends KinematicBody2D
+class_name Boulder
+
+func _ready() -> void:
+	pass
--- src/code/robot_player.gd
+++ src/code/robot_player.gd
@@ -9,6 +9,6 @@ func _physics_process(delta):
 	
 	for i in range(get_slide_count() - 1):
 		var collision = get_slide_collision(i)
-		
-		if collision.collider.get_class()=="KinematicBody2D":
+		print()
+		if collision.collider.get_collision_layer_bit(4)==true:
 			collision.collider.push(get_direction().x)
