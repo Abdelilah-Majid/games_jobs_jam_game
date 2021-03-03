ac09ee5d8347c461709f3b35dbbaf138fc3721d7
abdelilah majid <ain@pop-os.localdomain>
2021-03-03 21:15:29 +0100

    adding moving platforms

--- src/code/global_scripts/global_players_script.gd
+++ src/code/global_scripts/global_players_script.gd
@@ -7,3 +7,11 @@ var what_player_is_in_water:=""
 var robot_health=100.0
 var lighting_braziers=0
 var is_open_door_platforms_active=[]
+
+
+
+func _physics_process(delta: float) -> void:
+	if what_player_is_in_water=="robot":
+		pass
+	if robot_health<=0:
+		pass
