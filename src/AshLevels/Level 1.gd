265118723f1e91ef915ca1dc1671f43ac8c115c6
abdelilah majid <ain@pop-os.localdomain>
2021-03-10 14:12:15 +0100

    fixing sq code

--- src/AshLevels/Level 1.gd
+++ src/AshLevels/Level 1.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
 func _ready():
-	pass 
-	
-
-func _on_Soundtrack__body_entered(body):
 	MusicController.play_lvl1_music()
 	
--- src/AshLevels/Level 2.gd
+++ src/AshLevels/Level 2.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
 func _ready():
-	pass 
-
-func _on_Soundtrack_lvl2_body_entered(body):
 	MusicController.play_lvl2_music()
 	
